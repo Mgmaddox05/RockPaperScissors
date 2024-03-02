74115947c698f6c60d111d70795d058a0c36d2ce
Mgmaddox05 <125920278+Mgmaddox05@users.noreply.github.com>
2024-03-02 13:08:25 -0600

    Upadte 1.1

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection.toLowerCase()) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === 'rock' && computerSelection.toLowerCase() === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection.toLowerCase() === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection.toLowerCase() === 'paper')
+    ) {
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
